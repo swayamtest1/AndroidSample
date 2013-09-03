74ac4d1c7aef0573f446ffd613663bf4e64bbdb1
swayam <swayam.test1@gmail.com>
2013-09-03 20:36:02 +0530

    fifth commit...

---
+++ AndroidSample/src/com/example/androidsample/Second.java
@@ -0,0 +1,11 @@
+package com.example.androidsample;
+
+import android.app.Activity;
+
+public class Second extends Activity {
+
+	protected void onCreate(android.os.Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+	};
+}
